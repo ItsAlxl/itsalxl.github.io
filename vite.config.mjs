caa9cda9e05b881781c1f5d727865a5538763502
alxl <its.alxl@proton.me>
2025-04-07 18:01:19 -0500

    up deps, fix rollup

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { globSync } from 'glob'
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: globSync('src/**/*.html'),
+    },
   },
   plugins: [
     tailwindcss(),
